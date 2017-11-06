cc9da1cd9de9d9daad6c4909294b825976814d95
SmartHaung <464510690@qq.com>
2017-11-07 00:49:25 +0800

    生成叫号，还要研究下如何生成多个formid

--- pages/callqueue/callqueue.js
+++ pages/callqueue/callqueue.js
@@ -1,21 +1,67 @@
+var userInfo = getApp().globalData.userInfo
+var backendUrl = getApp().globalData.backendUrl
 Page({
   data: {
-    businessName: "测试商家",
+    businessName: "",
     businessId: "",
-    businessUniqueId: ""
+    businessUniqueId: "",
+    orderId: "",
+    callqueueFormids: ""
+  },
+  orderInput: function (e) {
+    this.setData({
+      orderId: e.detail.value
+    })
+  },
+  formSubmit: function (e) {
+    this.callqueueFormids += e.detail.formId + ","
+  },
+  createCallQueue: function () {
+    for (var i = 0; i < 10; i++) {
+      this.formSubmit();
+    }
+    wx.request({
+      url: backendUrl + '/consumer/addcallrequest',
+      data: {
+        callqueueBusinessId: this.businessId,
+        callqueueOrderid: this.orderId,
+        callqueueCreateuserUnionid: "1111",
+        callqueueFormids: this.callqueueFormids
+      },
+      success: function (res) {
+        var title = ""
+        if (res && res.data && res.data.id == 1) {
+          title = "提交成功"
+        } else {
+          title = "提交失败"
+        }
+        wx.showToast({
+          title: title,
+          icon: 'success',
+          duration: 2000
+        })
+      }
+    })
+
+
   },
   onLoad: function (options) {
+    var that = this;
     this.setData({
       businessId: options.businessId,
       businessUniqueId: options.businessUniqueId,
     })
     wx.request({
-      url: 'https://www.huangwenbin.xin/business/queryBusinessInfo',
+      url: backendUrl + '/business/queryBusinessInfo',
       data: {
-        businessId: 2,
+        businessId: options.businessId,
       },
       success: function (res) {
-        debugger
+        if (res && res.data && res.data.data && res.data.data.businessInfoName) {
+          that.setData({
+            businessName: res.data.data.businessInfoName
+          })
+        }
       }
     })
   }
