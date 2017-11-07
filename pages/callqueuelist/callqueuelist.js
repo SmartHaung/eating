fe0abe54837ddd2ba944ffa32414bccb4f6fa68b
SmartHaung <464510690@qq.com>
2017-11-07 22:16:38 +0800

    商家处理叫号

--- pages/callqueuelist/callqueuelist.js
+++ pages/callqueuelist/callqueuelist.js
@@ -1,13 +1,31 @@
 Page({
   data: {
     listData: [
-      { "code": "01", "text": "text1", "type": "type1" },
-      { "code": "02", "text": "text2", "type": "type2" },
-      { "code": "03", "text": "text3", "type": "type3" },
-      { "code": "04", "text": "text4", "type": "type4" },
-      { "code": "05", "text": "text5", "type": "type5" },
-      { "code": "06", "text": "text6", "type": "type6" },
-      { "code": "07", "text": "text7", "type": "type7" }
+      { "callqueueOrderid": "01", "text": "text1", "type": "type1" },
+      { "callqueueOrderid": "02", "text": "text2", "type": "type2" },
+      { "callqueueOrderid": "03", "text": "text3", "type": "type3" },
+      { "callqueueOrderid": "04", "text": "text4", "type": "type4" },
+      { "callqueueOrderid": "05", "text": "text5", "type": "type5" },
+      { "callqueueOrderid": "06", "text": "text6", "type": "type6" },
+      { "callqueueOrderid": "01", "text": "text1", "type": "type1" },
+      { "callqueueOrderid": "02", "text": "text2", "type": "type2" },
+      { "callqueueOrderid": "03", "text": "text3", "type": "type3" },
+      { "callqueueOrderid": "04", "text": "text4", "type": "type4" },
+      { "callqueueOrderid": "05", "text": "text5", "type": "type5" },
+      { "callqueueOrderid": "06", "text": "text6", "type": "type6" },
+      { "callqueueOrderid": "01", "text": "text1", "type": "type1" },
+      { "callqueueOrderid": "02", "text": "text2", "type": "type2" },
+      { "callqueueOrderid": "03", "text": "text3", "type": "type3" },
+      { "callqueueOrderid": "04", "text": "text4", "type": "type4" },
+      { "callqueueOrderid": "05", "text": "text5", "type": "type5" },
+      { "callqueueOrderid": "06", "text": "text6", "type": "type6" },
+      { "callqueueOrderid": "01", "text": "text1", "type": "type1" },
+      { "callqueueOrderid": "02", "text": "text2", "type": "type2" },
+      { "callqueueOrderid": "03", "text": "text3", "type": "type3" },
+      { "callqueueOrderid": "04", "text": "text4", "type": "type4" },
+      { "callqueueOrderid": "05", "text": "text5", "type": "type5" },
+      { "callqueueOrderid": "06", "text": "text6", "type": "type6" },
+      { "callqueueOrderid": "07", "text": "text7", "type": "type7" }
     ]
   },
   onLoad: function () {
